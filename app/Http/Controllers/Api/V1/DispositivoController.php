e47b8e5ee1b4758e580be7b08d7c018c59e6b192
carloantoniocc@gmail.com <carloantoniocc@gmail.com>
2022-08-06 23:16:27 -0400

    Configuracion de Laravel sanctum junto a la version 1 de api

---
+++ app/Http/Controllers/Api/V1/DispositivoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Dispositivo;
+use Illuminate\Http\Request;
+
+class DispositivoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dispositivo $dispositivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dispositivo $dispositivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dispositivo  $dispositivo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dispositivo $dispositivo)
+    {
+        //
+    }
+}
